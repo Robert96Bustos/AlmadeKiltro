b2fc4c5db628969f8dce1fcfb1d1a65f77bddb6f
Robert Bustos <robertbustos1996@gmail.com>
2022-05-14 15:12:23 -0400

    Modelo finalizado y creación de superuser

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Raza, Especie, Mascota, MascotaPerdida, MascotaEncontrada, MascotaAdopcion, FormularioAdopcion, Fundacion
 # Register your models here.
+
+
+
+admin.site.register(Raza)
+admin.site.register(Especie)
+admin.site.register(Mascota)
+admin.site.register(MascotaPerdida)
+admin.site.register(MascotaEncontrada)
+admin.site.register(MascotaAdopcion)
+admin.site.register(FormularioAdopcion)
+admin.site.register(Fundacion)
--- app/models.py
+++ app/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-# Opciones raza !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+opciones_nombre_raza = [
+    [0, "Kiltro"],
+    [1, "Mestizo"],
+    [2, "Poodle"],
+    [3, "Galgo"],
+    [4, "Otro"]
+]
+
 class Raza(models.Model):
-    nombre_raza = models.CharField(max_length=250)
+    nombre_raza = models.IntegerField(choices=opciones_nombre_raza)
 
     def __str__(self):
         return self.nombre_raza
@@ -16,15 +23,15 @@ opciones_especie = [
 ]
 
 class Especie(models.Model):
-    nombre_especie = models.IntegerField(choices=opciones_especies)
+    nombre_especie = models.IntegerField(choices=opciones_especie)
 
     def __str__(self):
         return self.nombre_especie
 
 # Opciones sexo de la mascota
 opciones_sexo = [
-    [0, "M"],
-    [1, "F"]
+    [0, "Hembra"],
+    [1, "Macho"]
 ]
 
 # Opciones tamaño de la mascota
@@ -36,6 +43,7 @@ opciones_tamaño = [
 
 class Mascota(models.Model):
     nombre_mascota = models.CharField(max_length=250)
+    imagen = models.ImageField(upload_to="mascota", null=True)
     edad_mascota = models.IntegerField()
     sexo = models.IntegerField(choices=opciones_sexo)
     descripcion = models.TextField()
@@ -48,41 +56,57 @@ class Mascota(models.Model):
         return self.nombre_mascota
 
 class MascotaPerdida(models.Model):
+    nombre_mascota_perdida = models.CharField(max_length=250)
     contacto_perdida = models.CharField(max_length=250)
     fecha_extravio = models.DateField()
     fecha_publicacion = models.DateField()
     mascota = models.ForeignKey(Mascota, on_delete=models.PROTECT)
 
     def __str__(self):
-        return self.
+        return self.nombre_mascota_perdida
 
 class MascotaEncontrada(models.Model):
+    nombre_mascota_encontrada = models.CharField(max_length=250)
     fecha_encontrada = models.DateField()
     fecha_publicacion = models.DateField()
     contacto_encontrada = models.CharField(max_length=250)
     mascota = models.ForeignKey(Mascota, on_delete=models.PROTECT)
 
     def __str__(self):
-        return self.
+        return self.nombre_mascota_encontrada
 
 class MascotaAdopcion(models.Model):
+    nombre_mascota_adopcion = models.CharField(max_length=250)
     peso = models.IntegerField()
     mascota = models.ForeignKey(Mascota, on_delete=models.PROTECT)
 
     def __str__(self):
-        return self.
+        return self.nombre_mascota_adopcion
+
+# Opciones tipo vivienda
+opciones_tipo_vivienda = [
+    [0, "Casa"],
+    [1, "Departamento"]
+]
+# Opciones tipo vivienda
+opciones_cantidad_mascotas = [
+    [0, "0"],
+    [1, "1"],
+    [2, "2"],
+    [3, "Más de 2"]
+]
 
 class FormularioAdopcion(models.Model):
     nombres = models.CharField(max_length=250)
     apellidos = models.CharField(max_length=250)
     edad = models.IntegerField()
     telefono = models.IntegerField()
-    tipo_vivienda = = models.IntegerField()
+    tipo_vivienda = models.IntegerField(choices=opciones_tipo_vivienda)
     direccion = models.CharField(max_length=250)
     otra_mascota = models.BooleanField()
     fecha_solicitud = models.DateField()
-    estado_solicitud = = models.IntegerField()
-    cantidad_mascotas = models.IntegerField()
+    estado_solicitud = models.IntegerField()
+    cantidad_mascotas = models.IntegerField(choices=opciones_cantidad_mascotas)
     mascota = models.ForeignKey(Mascota, on_delete=models.PROTECT)
 
     def __str__(self):
